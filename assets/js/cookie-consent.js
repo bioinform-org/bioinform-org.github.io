61831d2ae78f48eff52c167ce44f8f10695e3e78
Katarina <katarina.elez1@gmail.com>
2022-04-14 04:59:55 +0200

    Fix cookie consent for search

--- assets/js/cookie-consent.js
+++ assets/js/cookie-consent.js
@@ -22,9 +22,6 @@ window.cookieconsent.initialise({
       loadGAonConsent();
       loadAlgoliaOnConsent();
     }
-    if (type == 'opt-out' && !didConsent) {
-      // disable cookies
-    }
   },
   onStatusChange: function(status, chosenBefore) {
     var type = this.options.type;
@@ -34,19 +31,12 @@ window.cookieconsent.initialise({
       loadGAonConsent();
       loadAlgoliaOnConsent();
     }
-    if (type == 'opt-out' && !didConsent) {
-      // disable cookies
-    }
   },
   onRevokeChoice: function() {
     var type = this.options.type;
     if (type == 'opt-in') {
       // disable cookies
-    }
-    if (type == 'opt-out') {
-      // enable cookies
-      loadGAonConsent();
-      loadAlgoliaOnConsent();
+      unloadAlgoliaOnNonConsent()
     }
   }
 });
